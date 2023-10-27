3ae3789b630779923f63e1ad7be9d2e8b8f599d8
alexbirender <143655361+alexbirender@users.noreply.github.com>
2023-10-27 09:35:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+
 app = Flask(__name__)
+
+print(os.getenv('ENV'))
+print(os.getenv('DB'))
+print(os.getenv('DB222'))
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-import os
-print(os.getenv('ENV'))
-print(os.getenv('DB'))
-print(os.getenv('DB222'))
